07031eb0f05c1b2d7f928b9993358eea10c0aa85
Kevin Lee <k.lee@arts.ac.uk>
2024-01-14 12:32:35 +0000

    prettier

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,13 @@
 export const setUserActivities = (data) => {
-    return {
-        type: "SET_USER_ACTIVITIES",
-        payload: data,
-    };
+  return {
+    type: 'SET_USER_ACTIVITIES',
+    payload: data,
+  };
 };
 
 export const setUser = (data) => {
-    return {
-        type: "SET_USER",
-        payload: data,
-    };
+  return {
+    type: 'SET_USER',
+    payload: data,
+  };
 };
-
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,22 @@
-import { combineReducers } from "redux";
+import {combineReducers} from 'redux';
 
 const setUserReducer = (tokens = null, action) => {
-    switch (action.type) {
-        case "SET_USER":
-            return action.payload;
-        default:
-            return tokens;
-    }
+  switch (action.type) {
+    case 'SET_USER':
+      return action.payload;
+    default:
+      return tokens;
+  }
 };
 
 const setUserActivitiesReducer = (user = null, action) => {
-    if (action.type === "SET_USER_ACTIVITIES") {
-        return action.payload;
-    }
-    return user;
+  if (action.type === 'SET_USER_ACTIVITIES') {
+    return action.payload;
+  }
+  return user;
 };
 
 export default combineReducers({
-    returnTokens: setUserReducer,
-    user: setUserActivitiesReducer,
+  returnTokens: setUserReducer,
+  user: setUserActivitiesReducer,
 });
